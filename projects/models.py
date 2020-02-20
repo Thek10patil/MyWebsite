b2fa8929da9afc7d04451263e0f79ff21b48362c
Thek10patil <kkpatil1@asu.edu>
2020-02-19 21:00:43 -0700

    Updated about me tab

--- projects/models.py
+++ projects/models.py
@@ -2,48 +2,49 @@ from django.db import models
 
 
 class Projects(models.Model):
-	title = models.CharField(max_length = 200)
-	course_name = models.CharField(max_length = 200)
-	body = models.TextField()
-	year = models.CharField(max_length = 200)
-	created_at = models.CharField(max_length = 200)
-	githublink = models.CharField(max_length = 200)
-	technologyused = models.TextField()
-	instructor =  models.CharField(max_length = 200)
+    title = models.CharField(max_length=200)
+    course_name = models.CharField(max_length=200)
+    body = models.TextField()
+    year = models.CharField(max_length=200)
+    created_at = models.CharField(max_length=200)
+    githublink = models.CharField(max_length=200)
+    technologyused = models.TextField()
+    instructor = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.title
 
-	def __str__(self):
-		return self.title
-
-	class Meta:
-		verbose_name_plural = "Projects"
-
+    class Meta:
+        verbose_name_plural = "Projects"
 
 
 class Courses(models.Model):
-	course_name = models.CharField(max_length = 200)
-	created_at = models.CharField(max_length = 200)
-	def __str__(self):
-		return self.course_name
+    course_name = models.CharField(max_length=200)
+    created_at = models.CharField(max_length=200)
 
-	class Meta:
-		verbose_name_plural = "Courses"
+    def __str__(self):
+        return self.course_name
+
+    class Meta:
+        verbose_name_plural = "Courses"
+
 
 class Aboutme(models.Model):
-	about = models.TextField()
-	def __str__(self):
-		return 'Aboutme'
-	class Meta:
-		verbose_name_plural = "Aboutme"
+    about = models.TextField()
+
+    def __str__(self):
+        return 'Aboutme'
+
+    class Meta:
+        verbose_name_plural = "Aboutme"
+
 
 class Contacts(models.Model):
-	contact_details = models.CharField(max_length = 200)
-	contact_link = models.CharField(max_length = 200)	
-	def __str__(self):
-		return self.contact_details
-	class Meta:
-		verbose_name_plural = "Contacts"
+    contact_details = models.CharField(max_length=200)
+    contact_link = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.contact_details
 
-
-
+    class Meta:
+        verbose_name_plural = "Contacts"
--- projects/views.py
+++ projects/views.py
@@ -2,47 +2,51 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Projects, Courses, Aboutme, Contacts
 
+
 # Create your views here.
 def projects(request):
-	# return HttpResponse("<h1>ketan</h1>").
+    # return HttpResponse("<h1>ketan</h1>").
 
-	projects = Projects.objects.all()
+    projects = Projects.objects.all()
 
-	context ={
-		'title':'Projects',
-		'projects':projects
-	}
+    context = {
+        'title': 'Projects',
+        'projects': projects
+    }
 
-	return render(request, 'projects/index.html', context)
+    return render(request, 'projects/index.html', context)
 
 
 def details(request, id):
-	project = Projects.objects.get(id=id)
+    project = Projects.objects.get(id=id)
 
-	context = {
-		'project':project
-	}
+    context = {
+        'project': project
+    }
 
-	return render(request, "projects/details.html", context)
+    return render(request, "projects/details.html", context)
+
 
 def courses(request):
-	courses = Courses.objects.all()
+    courses = Courses.objects.all()
 
-	context ={
-		'courses':courses
-	}
-	return render(request, "projects/courses.html", context)
+    context = {
+        'courses': courses
+    }
+    return render(request, "projects/courses.html", context)
+
 
 def aboutme(request):
-	about = Aboutme.objects.all()
-	context = {
-		'about':about
-	}
-	return render(request, "projects/aboutme.html", context)
+    about = Aboutme.objects.all()
+    context = {
+        'about': about
+    }
+    return render(request, "projects/aboutme.html", context)
+
 
 def contact(request):
-	contacts = Contacts.objects.all()
-	context = {
-		'contact':contacts
-	}
-	return render(request, "projects/contact.html", context)
+    contacts = Contacts.objects.all()
+    context = {
+        'contact': contacts
+    }
+    return render(request, "projects/contact.html", context)
